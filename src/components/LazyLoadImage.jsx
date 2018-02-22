df47147fa35d36210afb0b5880921b980af06be1
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-02-22 21:09:45 +0100

    Make LazyLoadImage a PureComponent

--- src/components/LazyLoadImage.jsx
+++ src/components/LazyLoadImage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-class LazyLoadImage extends React.Component {
+class LazyLoadImage extends React.PureComponent {
   constructor(props) {
     super(props);
 
