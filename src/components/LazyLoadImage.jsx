823d7a60c983ee65ac17a4b8fe126f33087492d9
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-02-24 09:42:10 +0100

    Make LazyLoadImage a regular Component because we are implementing shouldComponentUpdate manually

--- src/components/LazyLoadImage.jsx
+++ src/components/LazyLoadImage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-class LazyLoadImage extends React.PureComponent {
+class LazyLoadImage extends React.Component {
   constructor(props) {
     super(props);
 
